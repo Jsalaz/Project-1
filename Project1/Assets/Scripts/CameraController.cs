760139a20a25249e5e3514ac4d3d719a29f35c46
Jsalaz <jorgesalazar.cs@gmail.com>
2016-10-18 10:11:26 -0400

    Camera Script Added -JS

---
+++ Project1/Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject gameBall;
+
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - gameBall.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void LateUpdate (){
+		transform.position = gameBall.transform.position + offset;
+	}
+}
