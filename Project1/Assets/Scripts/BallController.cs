4dcb0c99ed6416d68b6ab9301fbd051c2a8b9e4d
Jsalaz <jorgesalazar.cs@gmail.com>
2016-10-18 08:35:41 -0400

    First True Commit with level 1 scene and ballController Script

---
+++ Project1/Assets/Scripts/BallController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallController : MonoBehaviour {
+
+	public float speed = 5;
+	private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		//Debug.Log ("hi");
+	}
+
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Debug.Log (moveHorizontal + " " + moveVertical);
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement * speed);
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
