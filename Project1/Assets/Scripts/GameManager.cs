dc7d0ffc9a0943685f2b6948e912969601b5fb19
sportsziggy <sportsziggy@aol.com>
2016-10-19 13:08:00 -0400

    added view script

--- Project1/Assets/Scripts/GameManager.cs
+++ Project1/Assets/Scripts/GameManager.cs
@@ -1,18 +1,67 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+public enum GameState
+{
+    //menu,
+    inGame,
+    gameOver,
+}
+
 public class GameManager : MonoBehaviour
 {
-    public GameManager instance;
+    public Canvas inGameCanvas;
+    //public Canvas menuCanvas;
+    public Canvas gameOverCanvas;
+    public static GameManager instance;
+    public GameState currentGameState = GameState.inGame; // = GameState.menu;
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Awake ()
     {
         instance = this;
 	}
-	
-	// Update is called once per frame
-	void Update ()
+
+    void Start()
+    {
+        //currentGameState = GameState.menu;
+    }
+
+    void SetGameState(GameState newGameState)
+    {
+        //if (newGameState == GameState.menu)
+        //{
+        //    //setup Unity scene for menu state
+        //    //menuCanvas.enabled = true;
+        //    inGameCanvas.enabled = false;
+        //    gameOverCanvas.enabled = false;
+        //}
+        if (newGameState == GameState.inGame)
+        {
+            //setup Unity scene for inGame state
+            //menuCanvas.enabled = false;
+            inGameCanvas.enabled = true;
+            gameOverCanvas.enabled = false;
+        }
+        else if (newGameState == GameState.gameOver)
+        {
+            //setup Unity scene for gameOver state
+            //menuCanvas.enabled = false;
+            inGameCanvas.enabled = false;
+            gameOverCanvas.enabled = true;
+        }
+        //else if (newGameState == GameState.levelTwo)
+        //{
+        //    menuCanvas.enabled = false;
+        //    inGameCanvas.enabled = false;
+        //    GameOverCanvas.enabled = false;
+        //    level2Canvas.enabled = true;
+        //}
+        currentGameState = newGameState;
+    }
+
+    // Update is called once per frame
+    void Update ()
     {
 	
 	}
---
+++ Project1/Assets/Scripts/ViewInGame.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+public class ViewInGame : MonoBehaviour
+{
+    public Text scoreLabel;
+
+    public static ViewInGame instance;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    void Update()
+    {
+        //if (GameManager.instance.currentGameState == GameState.inGame)
+        //{
+            scoreLabel.text = Mathf.Round(Time.time).ToString();
+        //}
+    }
+}
